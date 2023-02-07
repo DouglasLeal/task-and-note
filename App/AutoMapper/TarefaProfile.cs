dead970591348b7d61c5c185298597285b09bcd0
Douglas Leal <douglas.leal@live.com>
2023-02-07 13:40:33 -0300

    Adicionado AutoMapper e TarefaProfile criado

---
+++ App/AutoMapper/TarefaProfile.cs
@@ -0,0 +1,15 @@
+﻿using App.Models;
+using App.ViewModels;
+using AutoMapper;
+
+namespace App.AutoMapper
+{
+    public class TarefaProfile : Profile
+    {
+        public TarefaProfile()
+        {
+            CreateMap<Tarefa, TarefaViewModel>();
+            CreateMap<TarefaViewModel, Tarefa>();
+        }
+    }
+}
