3731700f4f4e2386072eaa7cb679a72a218b822f
Douglas Leal <douglas.leal@live.com>
2023-02-07 13:28:24 -0300

    TarefaViewModel criado.

---
+++ App/ViewModels/TarefaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace App.ViewModels
+{
+    public class TarefaViewModel
+    {
+        public class Tarefa
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+            [StringLength(255, ErrorMessage = "O campo {0} não pode ter menos do que {2} e mais do que {1} caracteres.", MinimumLength = 3)]
+            [DisplayName("Conteúdo")]
+            public string? Conteudo { get; set; }
+
+            [DisplayName("Concluído")]
+            public bool Concluido { get; set; }
+
+            public string? AspNetUserId { get; set; }
+            public IdentityUser? AspNetUser { get; set; }
+        }
+    }
+}
