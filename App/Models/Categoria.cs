c0dc97880d95cf83a54dd18a20c3253e05e03690
Douglas Leal <douglas.leal@live.com>
2023-02-08 15:18:03 -0300

    Model Categoria criado

---
+++ App/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MinLength(1, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MinLength(1, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        public string? Slug { get; set; }
+    }
+}
