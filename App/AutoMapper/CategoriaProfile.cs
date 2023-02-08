355ab855225335214430c41330329b1b490d6daf
Douglas Leal <douglas.leal@live.com>
2023-02-08 15:23:18 -0300

    CategoriaProfile criado para AutoMapper

---
+++ App/AutoMapper/CategoriaProfile.cs
@@ -0,0 +1,15 @@
+﻿using App.Models;
+using App.ViewModels;
+using AutoMapper;
+
+namespace App.AutoMapper
+{
+    public class CategoriaProfile : Profile
+    {
+        public CategoriaProfile()
+        {
+            CreateMap<Categoria, CategoriaViewModel>();
+            CreateMap<CategoriaViewModel, Categoria>();
+        }
+    }
+}
