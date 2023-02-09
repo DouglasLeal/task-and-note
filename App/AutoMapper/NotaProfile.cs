fd03eb1f29e43d8c13fc152cdb51bba473ff9cf1
Douglas Leal <douglas.leal@live.com>
2023-02-08 23:11:04 -0300

    NotaProfile para AutoMapper criado

---
+++ App/AutoMapper/NotaProfile.cs
@@ -0,0 +1,15 @@
+﻿using App.Models;
+using App.ViewModels;
+using AutoMapper;
+
+namespace App.AutoMapper
+{
+    public class NotaProfile : Profile
+    {
+        public NotaProfile()
+        {
+            CreateMap<Nota, NotaViewModel>();
+            CreateMap<NotaViewModel, Nota>();
+        }
+    }
+}
